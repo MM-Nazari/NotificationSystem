157979437be19a6f54a36382d43ecfc3317dd72b
Mohammad Mehdi Nazari <nazarimohammadmehdi6@gmail.com>
2025-08-11 20:38:40 +0330

    Add files via upload

---
+++ Notification.Infrastructure/Log/NotificationLogger.cs
@@ -0,0 +1,25 @@
+﻿using Notification.Core.Entities;
+using Notification.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notification.Infrastructure.Log
+{
+    /// <summary>
+    /// Logs notification details (e.g., to console, file, or DB).
+    /// </summary>
+    public class NotificationLogger : INotificationLogger
+    {
+        /// <summary>
+        /// Logs the given notification.
+        /// </summary>
+        /// <param name="log">Notification log detail</param>
+        public void Log(NotificationLog log)
+        {
+            // TODO: Implement logging (e.g., console, file, or database)
+        }
+    }
+}
